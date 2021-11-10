f70e0402c5d5b0e2e9d2c909575ab1f6a67aa317
hellcy <chengyuan82281681@hotmail.com>
2021-11-10 23:56:25 +1100

    added data.sql and using JDBC datasource initializer

--- src/main/java/com/yuancheng/springrecipeapp/models/UnitOfMeasure.java
+++ src/main/java/com/yuancheng/springrecipeapp/models/UnitOfMeasure.java
@@ -2,13 +2,14 @@ package com.yuancheng.springrecipeapp.models;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class UnitOfMeasure {
 
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String description;
 
