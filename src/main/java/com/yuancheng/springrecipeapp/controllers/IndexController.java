d50f6714927879bb2e29c97a94fa15f7fdd88d3c
hellcy <chengyuan82281681@hotmail.com>
2021-11-11 22:55:29 +1100

    added IndexControllerTest

--- src/main/java/com/yuancheng/springrecipeapp/controllers/IndexController.java
+++ src/main/java/com/yuancheng/springrecipeapp/controllers/IndexController.java
@@ -1,16 +1,10 @@
 package com.yuancheng.springrecipeapp.controllers;
 
-import com.yuancheng.springrecipeapp.models.Category;
-import com.yuancheng.springrecipeapp.models.UnitOfMeasure;
-import com.yuancheng.springrecipeapp.repositories.CategoryRepository;
-import com.yuancheng.springrecipeapp.repositories.UnitOfMeasureRepository;
 import com.yuancheng.springrecipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Slf4j
 @Controller
---
+++ src/test/java/com/yuancheng/springrecipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,43 @@
+package com.yuancheng.springrecipeapp.controllers;
+
+import com.yuancheng.springrecipeapp.repositories.RecipeRepository;
+import com.yuancheng.springrecipeapp.services.RecipeService;
+import com.yuancheng.springrecipeapp.services.RecipeServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+class IndexControllerTest {
+
+  @Mock
+  RecipeService recipeService;
+
+  @Mock
+  Model model;
+
+  IndexController indexController;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+    indexController = new IndexController(recipeService);
+  }
+
+  @Test
+  void getIndexPage() {
+    String viewName = indexController.getIndexPage(model);
+
+    verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    verify(recipeService, times(1)).getRecipes();
+
+    assertEquals("index", viewName);
+  }
+}
