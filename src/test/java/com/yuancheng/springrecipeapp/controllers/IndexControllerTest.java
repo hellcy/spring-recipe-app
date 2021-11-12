67cf647d8c75901e7c2d28f27383e4b1b9bc7e26
hellcy <chengyuan82281681@hotmail.com>
2021-11-12 18:24:55 +1100

    added MockMVC test for IndexController

--- src/test/java/com/yuancheng/springrecipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/yuancheng/springrecipeapp/controllers/IndexControllerTest.java
@@ -9,6 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -33,6 +38,15 @@ class IndexControllerTest {
   void setUp() {
     MockitoAnnotations.openMocks(this);
     indexController = new IndexController(recipeService);
+  }
+
+  @Test
+  void testMockMVC() throws Exception {
+    // standalone doesn't require Spring Context
+    MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+    mockMvc.perform(MockMvcRequestBuilders.get("/"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.view().name("index"));
   }
 
   @Test
