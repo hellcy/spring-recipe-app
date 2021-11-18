449704642bdb18392949e8b2dbab36bc36dd66ba
hellcy <chengyuan82281681@hotmail.com>
2021-11-19 00:19:05 +1100

    adding ExceptionHandler and 404error.html

--- src/main/java/com/yuancheng/springrecipeapp/controllers/RecipeController.java
+++ src/main/java/com/yuancheng/springrecipeapp/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.yuancheng.springrecipeapp.controllers;
 
 import com.yuancheng.springrecipeapp.commands.RecipeCommand;
+import com.yuancheng.springrecipeapp.exceptions.NotFoundException;
 import com.yuancheng.springrecipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -50,5 +53,14 @@ public class RecipeController {
     recipeService.deleteById(Long.valueOf(id));
 
     return "redirect:/";
+  }
+
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ExceptionHandler(NotFoundException.class)
+  public ModelAndView handleNotFound() {
+    log.error("Handling not found exception");
+    ModelAndView mav = new ModelAndView();
+    mav.setViewName("404error");
+    return mav;
   }
 }
